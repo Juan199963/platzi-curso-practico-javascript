f435456e49bfc7bf154c3aeb7756142ec73b635e
Juan Carlos Chambi Python Dev <juan199963@gmail.com>
2022-01-02 09:12:52 -0500

    SE AÑADIO LA FIGURA TRIANGULO

--- figuras_copy.js
+++ figuras_copy.js
@@ -14,3 +14,24 @@ const areacuadrado = ladocuadrado*ladocuadrado
 console.log('El perimetro del cuadrado es: ' + areacuadrado)
 
 console.groupEnd
+
+console.group("Triangulos");
+
+
+const ladotriangulo1 = 6;
+const ladotriangulo2 = 6;
+const basetriangulo1 = 4;
+const alturatriangulo1 = 5.5;
+
+
+console.log("Los lados del triangulo miden: " + ladotriangulo1 +","+ladotriangulo2 +"y "+ basetriangulo1 );
+
+const perimetrotriangulo = ladotriangulo1+ladotriangulo2+basetriangulo1
+
+console.log('El perimetro del triangulo es: ' + perimetrotriangulo)
+
+const areacuadrado = (basetriangulo1*alturatriangulo1)/2
+
+console.log('La altura del triangulo es: ' + alturatriangulo1)
+
+console.groupEnd
