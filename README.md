ce1c2e282f417193234866d0f5b5a36a20329cd1
Juan Carlos Chambi Python Dev <juan199963@gmail.com>
2022-01-02 08:03:56 -0500

    añadimos el archivo para probar

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # platzi-curso-practico-javascript
+
+
+## TALLER #1 : FIGURAS GEOMETRICAS
+
+-PRIMER PASO: Definir formulas
+-SEGUNDO: Implementar formulas en Javascript
+-TERCER: Crear funciones 
+-CUARTO: Integrar con HTML
---
+++ figuras_copy.js
@@ -0,0 +1,11 @@
+const ladocuadrado = 5;
+
+console.log("Los lados del cuadrado miden: " + ladocuadrado);
+
+const perimetrocuadrado = ladocuadrado*4
+
+console.log('El perimetro del cuadrado es: ' + perimetrocuadrado)
+
+const areacuadrado = ladocuadrado*ladocuadrado
+
+console.log('El perimetro del cuadrado es: ' + areacuadrado)
